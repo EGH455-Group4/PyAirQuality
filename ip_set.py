8cf39cec275e1819b5458549c5a1f8b6ff3412bf
Tom Bowyer <tom4310@gmail.com>
2023-08-26 12:04:03 +1000

    Clean up for lint

--- ip_set.py
+++ ip_set.py
@@ -1,5 +1,6 @@
 '''This script is a standalone command which will update the LCD screen to the IP address'''
 import logging
+import sys
 
 from config.config import Config
 from config.log import setup_logging
@@ -25,7 +26,7 @@ def main():
     logging.info("ip address is %s", local_ip_address)
 
     if local_ip_address == "":
-        exit(1)
+        sys.exit(1)
 
     service = Service(
         config,
@@ -38,8 +39,7 @@ def main():
 
     service.update_lcd_screen()
 
-    exit(0)
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
-
